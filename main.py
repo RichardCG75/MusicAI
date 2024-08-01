017b149f7c79da3295088267d5495a2e0d97f2fc
Ricardo Chacon <ricardo.chaconr6@gmail.com>
2024-07-31 18:58:00 -0600

    v1.0 generating music

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ import random
 import model
 import numpy as np
 
-
+# Generate aset of songs melody with AI
 seq_length = 32
 melody = model.generate_melody(model.model_pitch, model.model_duration, model.seed_sequence_pitch, model.seed_sequence_duration, seq_length)
 play.play_melody(melody)
 
-random_melody = random.choice(data.songs)
-random_melody = utils.apply_counterpoint_melodic_rules(random_melody)
-random_melody = utils.apply_counterpoint_rythm_rules(random_melody)
+# Apply important counterpoint rules
+melody = random.choice(data.songs)
+melody = utils.apply_counterpoint_melodic_rules(random_melody)
+melody = utils.apply_counterpoint_rythm_rules(random_melody)
 
+# Play the melody
 play.play_melody(random.choice(data.songs), speed=2)
